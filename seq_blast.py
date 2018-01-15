a3d8d02d56f76fe16a49f83d18870a8e75191aa3
Drew X <drewxdvst@outlook.com>
2018-01-15 12:43:56 +0200

    seq_blast.py is utility for blasting query sequences

---
+++ seq_blast.py
@@ -0,0 +1,60 @@
+#! /usr/bin/python
+from Bio.Blast import NCBIWWW
+from Bio import SeqIO
+import argparse
+import time
+import sys
+import os
+
+
+
+def get_sequences(query_sequences, fmt, output_dir):
+
+    with open(query_sequences) as query_fp:
+        seq_data = SeqIO.parse(query_fp, fmt)
+        i = 0
+        for seq in seq_data:
+            if not seq.seq:
+                continue
+            result_id = '.'.join(['_' + str(i),'blast'])
+            fname = ''.join([seq.id.replace('.ab1', ''), result_id])
+            dest_dir = output_dir
+            try:
+                os.mkdir(dest_dir)
+            except OSError:
+                pass
+            save_fname = os.path.join(dest_dir, fname)
+            if  os.path.exists(save_fname):
+                i += 1
+                print i,save_fname,'File exists...'
+                continue
+            results_handle = None
+            fail = 0
+            while not results_handle:
+                try:
+                    results_handle = NCBIWWW.qblast("blastn", "nt", seq.format(fmt), hitlist_size=10)
+                except Exception as e:
+                    fail += 1
+                    if fail == 15:
+                       print "Failed to connect to NCBIWWW server\nExiting...\n"
+                       sys.exit(1)
+                    print e
+                    time.sleep(5)
+                    print "Reconnecting to NCBIWWW server..."
+                    continue
+            with open(save_fname,'w') as save:
+                 save.write(results_handle.read())
+            i += 1
+            print i,save_fname
+
+if __name__ ==  '__main__':
+    
+    parser = argparse.ArgumentParser(description="blast sequences against NCBIWWW online server")
+    parser.add_argument('query_sequences', action='store', type=str,
+                        help='sequencesed or unsequencesed query sequence')
+    parser.add_argument('-f','--format', dest='fmt', action='store', type=str, required=False,
+                        default = "fasta",  help='format of input file')
+    parser.add_argument('-o','--ouput-dir', dest='output_dir', action='store', type=str, required=False,
+                        default = "blast_results",  help='output directory')    
+    args = parser.parse_args()
+    get_sequences(args.query_sequences, args.fmt, args.output_dir)
