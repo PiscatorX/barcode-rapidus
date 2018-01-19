16c3a139443e1a4e6adaef20c333224d41d8b82c
Drew X <drewxdvst@outlook.com>
2018-01-19 09:56:30 +0200

    Added option to save removed sequences

--- rm_seqs.py
+++ rm_seqs.py
@@ -9,6 +9,7 @@ def get_seqs(args):
         
     to_save = []
     rm_list = False
+    rm_to_save = []
     
     if args.rm_list: 
        rm_list = open(args.rm_list).read().splitlines()
@@ -21,25 +22,31 @@ def get_seqs(args):
            ref = seq.id
            if ref  in rm_list:
                print 'removed ',seq.id
+               rm_to_save.append(seq)
                continue
            to_save.append(seq)
-        
-           
-    if not args.out_format:
-        args.out_format =  args.in_format
     
-    if args.out_file:
-        out_file  = args.out_file
-    else:
-        out_file = 'pruned_'+args.in_file.replace(args.in_format, args.out_format)
         
     if any(to_save):
-        SeqIO.write(to_save, open(out_file,'w'), args.out_format)
+        if not args.out_format:
+            args.out_format =  args.in_format
+        if args.out_file:
+            out_file  = args.out_file
+        else:
+            out_file = 'pruned_'+args.in_file.replace(args.in_format, args.out_format)
+            SeqIO.write(to_save, open(out_file,'w'), args.out_format)
+
     
-            
+    if args.rm_save and any(rm_to_save):
+        if args.rm_out_file:
+            out_file  = args.rm_out_file
+        else:
+            out_file = 'removed_'+args.in_file.replace(args.in_format, args.out_format)
+        
+        
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description="""remove sequences from file""")
+    parser = argparse.ArgumentParser(description="""remove sequences from file using accession identifiers""")
     parser.add_argument('in_file', action='store', type=str)
     parser.add_argument('-o','--out-file', dest='out_file', 
                         action='store', type=str)    
@@ -47,8 +54,14 @@ if __name__ == '__main__':
                         action='store', default="fasta", required=False, type=str)
     parser.add_argument('-f','--out-format', dest='out_format', 
                         action='store', required=False, type=str)
+    parser.add_argument('-s','--save-removed', dest='rm_save', 
+                        action='store', required=False, type=str)
+    parser.add_argument('-R','--rm-outfile', dest='rm_out_file', 
+                        action='store', required=False, type=str)
     group = parser.add_mutually_exclusive_group(required=True)    
     group.add_argument('-r','--remove-list', dest='rm_list', action='store', required=False, type=str)
     group.add_argument('-a','--acc-list', dest='acc_list', action='store', nargs='+', required=False, type=str)
     args = parser.parse_args()
+    if args.rm_out_file:
+        args.rm_save = True
     get_seqs(args)
