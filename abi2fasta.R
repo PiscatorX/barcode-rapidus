7e57bf67f29bca7ef46a6276997ed44ac82ff7f0
Drew X <drewxdvst@outlook.com>
2018-02-07 14:20:22 +0200

    fix file renaming

--- abi2fasta.R
+++ abi2fasta.R
@@ -15,61 +15,44 @@ parser$add_argument("-t","--trim-cutoff", dest="trim_cutoff", default=0.015,
                     help="cutoff for sequencing trimming")
 args <- parser$parse_args()
 
-require(ape)
-require(reshape2)
-require(phangorn)
-require(stringi)
-require(stringr)
-require(ggplot2)
-#require(here)
-require(grid)
-require(gridExtra)
-# Bioconductor packages
-require(DECIPHER)
-require(Biostrings)
-require(sangerseqR)
-require(tools)
 #github("roblanf/sangeranalyseR")
 require(sangeranalyseR)
 
 
-
-
 setwd(cwd)
 input_folder <- normalizePath(args$input_folder)
-input_folder <- "/home/drewx/Documents/sanpccX/TestDev"
 forward_suffix <- args$forward_suffix
 reverse_suffix <- args$reverse_suffix
 split_char <- args$split_char
 
-
-read_sets <- make.readsets(input_folder, 
+read_sets <- make.consensus.seqs(input_folder, 
               forward_suffix, 
-              reverse_suffix, 
+              reverse_suffix,
+              min.reads = 0,
               trim = TRUE, 
               trim.cutoff = 0.015)
 
-
-base_seq_ids = character(length(read_sets$readsets))
+base_seq_ids = character(length(read_sets$consensus.sequences))
 i = 1
-for (seq_name in names(read_sets$readsets)){
+for (seq_name in names(read_sets$consensus.sequences)){
   seq_id <- basename(seq_name)
+  print(seq_id)
   new_seq_id <- paste(strsplit(seq_id, split_char)[[1]][[1]], seq_id)
   base_seq_ids[i] <- new_seq_id
   i <- i + 1
 }
 
-names(read_sets$readsets) <- base_seq_ids
+names(read_sets$consensus.sequences) <- base_seq_ids
 
-read_sets$readsets
-read_sets$read.summaries
-
+read_sets$consensus.summaries
+#read_sets$consensus.sequences
+BrowseSeqs(read_sets$consensus.sequences)
+ 
 fname = paste0(basename(input_folder),"_reads",".fasta")
 setwd(args$output_folder)
-write.dna(read_sets$readsets,
-          file = fname,
-          format = 'fasta', 
-          nbcol = -1, 
-          colsep = "", 
-          colw = 10000000)
-
+write.dna(read_sets$consensus.sequences,
+            file = fname,
+           format = 'fasta',
+           nbcol = -1,
+           colsep = "",
+           colw = 10000000)
