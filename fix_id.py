ffe6ccf56decf80fcbd290ce78f09dd7465609a1
Drew X <drewxdvst@outlook.com>
2018-02-12 17:55:46 +0200

    add change split char

--- fix_id.py
+++ fix_id.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
+import argparse
 
 from Bio import SeqIO
 import sys
 import os
 
-def fix_id(fasta_file):
+def fix_id(fasta_file, spliton):
 
     fixed_fasta = os.path.join(os.path.dirname(fasta_file),
                                     'fixed_'+os.path.basename(fasta_file))
@@ -13,12 +14,17 @@ def fix_id(fasta_file):
         fasta_seqs  = SeqIO.parse(fasta_object, 'fasta')
         new_seqs = []
         for i,seq  in enumerate(fasta_seqs, 1):
-            split_id = seq.id.split('_',1)[0]
+            split_id = seq.id.split(spliton,1)[0]
             #[0].split('-')[0]
             print i,split_id
             seq.id = split_id
             new_seqs.append(seq)
         SeqIO.write(new_seqs, fixed_fp, 'fasta')
     fixed_fp.close()
-
-fix_id(sys.argv[1])
+if  __name__ == '__main__':
+    parser = argparse.ArgumentParser(description = """fix sequences id by splitting on provide char""")
+    parser.add_argument('fasta_file',action='store', type=str, help="sequence in fasta format")
+    parser.add_argument('-s','--spliton', default = '_', type=str, required=False,
+                        help='split char for sequence id')
+    args  =  parser.parse_args()
+    fix_id(args.fasta_file, args.spliton)
