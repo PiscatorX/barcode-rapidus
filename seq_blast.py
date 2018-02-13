1d018fb7eb25beec49c0ebc3f0b63c7295f67f76
Drew X <drewxdvst@outlook.com>
2018-02-13 15:27:06 +0200

    fix document issues

--- seq_blast.py
+++ seq_blast.py
@@ -6,6 +6,7 @@ import argparse
 import time
 import sys
 import os
+
 __author__ = "Andrew Ndhlovu"
 __copyright__ = "Copyright 2018"
 __license__ = "GPL"
@@ -16,7 +17,7 @@ __email__ = "drewxdvst@outlook.com"
 
 
 
-def get_sequences(query_sequences, fmt, output_dir):
+def get_sequences(query_sequences, fmt, output_dir, hitlist_size, max_queries):
 
     with open(query_sequences) as query_fp:
         seq_data = SeqIO.parse(query_fp, fmt)
@@ -40,7 +41,7 @@ def get_sequences(query_sequences, fmt, output_dir):
             fail = 0
             while not results_handle:
                 try:
-                    results_handle = NCBIWWW.qblast("blastn", "nt", seq.format(fmt), hitlist_size=10)
+                    results_handle = NCBIWWW.qblast("blastn", "nt", seq.format(fmt), hitlist_size=hitlist_size)
                 except Exception as e:
                     fail += 1
                     if fail == 15:
@@ -54,15 +55,26 @@ def get_sequences(query_sequences, fmt, output_dir):
                  save.write(results_handle.read())
             i += 1
             print i,save_fname
+            if i ==  max_queries:
+                print "Max queries reached! Exiting..."
+                sys.exit(0)
+                
+            time.sleep(5)
+            
 
 if __name__ ==  '__main__':
     
     parser = argparse.ArgumentParser(description="blast sequences against NCBIWWW online server")
     parser.add_argument('query_sequences', action='store', type=str,
-                        help='sequencesed or unsequencesed query sequence')
+                        help='sequenced or unsequenced query sequence')
     parser.add_argument('-f','--format', dest='fmt', action='store', type=str, required=False,
                         default = "fasta",  help='format of input file')
     parser.add_argument('-o','--ouput-dir', dest='output_dir', action='store', type=str, required=False,
-                        default = "blast_results",  help='output directory')    
+                        default = "blast_results",  help='output directory')
+    parser.add_argument('-s','--hitlist-size', dest='hitlist_size', action='store', type=int, required=False,
+                        default =1,  help='output directory')
+    parser.add_argument('-m','--max-queries', dest='max_queries', action='store', type=int, required=False,
+                        default =100)
     args = parser.parse_args()
-    get_sequences(args.query_sequences, args.fmt, args.output_dir)
+    get_sequences(args.query_sequences, args.fmt, args.output_dir, args.hitlist_size, args.max_queries)
+                        
