aa069434226f4641dd419973e6460ab58d81ee66
Andrew Ndhlovu <drewxdvst@outlook.com>
2019-07-15 10:43:54 +0200

    fixed description

--- save_ids.py
+++ save_ids.py
@@ -12,7 +12,7 @@ __email__ = "drewxdvst@outlook.com"
 
 def show_ids():
     
-    parser = argparse.ArgumentParser(description="remove duplicate sequences and save list of seq ids", epilog="Requires Biopython")
+    parser = argparse.ArgumentParser(description="save list of seq ids", epilog="Requires Biopython")
     parser.add_argument('infile', action='store', type=str)
     parser.add_argument('-f','--format', dest='in_format', action='store', default='gb', required=False, type=str)
     args = parser.parse_args()
@@ -20,8 +20,8 @@ def show_ids():
     with open(args.infile+'.ids','w') as fp:
         seqs = SeqIO.parse(open(args.infile), args.in_format)
         for rec  in seqs:
-            print >>fp,'# '+rec.id
-            print rec.id
+            print('# '+rec.id, file=fp)
+            print(rec.id)
     
 if  __name__ == '__main__':
     show_ids()
