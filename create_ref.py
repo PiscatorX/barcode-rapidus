8c325a92c32170fb9b29ec6e5b6d552b9dbad921
Andrew Ndhlovu <drewxdvst@outlook.com>
2019-07-15 10:44:34 +0200

    fixe file path issue

--- create_ref.py
+++ create_ref.py
@@ -1,26 +1,44 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 from Bio import  SeqIO
 import argparse
 import sys
+import os
+
+__author__ = "Andrew Ndhlovu"
+__copyright__ = "Copyright 2018"
+__license__ = "GPL"
+__version__ = "3"
+__maintainer__ = "Andrew Ndhlovu"
+__email__ = "drewxdvst@outlook.com"
+
+
 
 def  get_ref(args):
      
      ref_obj = open(args.infile + '.ref','w')
-     seq_obj = open('final_' + args.infile.replace(args.in_format,'') + args.out_format, 'w')
+     path, fname = os.path.split(args.infile)
+     fname = 'final_' + fname.replace(args.in_format,'') + args.out_format
+     fname = os.path.join(path, fname)
+     seq_obj = open(fname, 'w')
+     args.uniprot = True
      with open(args.infile) as fp:
          seq_data = SeqIO.parse(fp, args.in_format)
          for i,record in enumerate(seq_data, 1):
-             if  args.in_format == 'gb':
-                  organism = record.annotations['organism']
-             else:
-                  organism = ' '.join(record.description.split()[1:3])
+             if args.uniprot:
+                  organism = record.description.split("OS=")[-1].split("OX=")[0].strip()
+             else: 
+                  if  args.in_format == 'gb':
+                      organism = record.annotations['organism']
+                  else:
+                      organism = ' '.join(record.description.split()[1:3])
+
              refs ="{}\t{}".format(record.id, organism)     
-             print >>ref_obj,refs
-             print refs
+             print(refs, file=ref_obj)
+             print(refs)
              record.description = ''
              SeqIO.write(record, seq_obj , args.out_format)
-     map(lambda f : f.close, [ref_obj,  seq_obj])
+     list(map(lambda f : f.close, [ref_obj,  seq_obj]))
 
      
 if  __name__ ==  '__main__':
@@ -28,5 +46,6 @@ if  __name__ ==  '__main__':
     parser.add_argument('infile', action='store', type=str)
     parser.add_argument('-f','--format', dest='in_format', action='store', default='gb', required=False, type=str)
     parser.add_argument('-o','--out_format', dest='out_format', action='store', default='fasta', required=False, type=str)
+    parser.add_argument('-u','--uniprot', action='store_true', )
     get_ref(parser.parse_args())
 
